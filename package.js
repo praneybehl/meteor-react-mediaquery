8590247db23f78e7b8e5173c39937187ee48f42f
Praney Behl <praneybehl@gmail.com>
2015-11-06 21:27:33 +1100

    updated package dependencies

--- package.js
+++ package.js
@@ -1,20 +1,23 @@
 Package.describe({
   name: 'praneybehl:react-mediaquery',
-  version: '0.0.1',
-  summary: '',
-  git: '',
+  version: '0.0.10',
+  summary: 'React Media queries component for responsive design packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-mediaquery',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('react-mediaquery.js');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.export('MediaQuery');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-mediaquery');
-  api.addFiles('react-mediaquery-tests.js');
+
+Npm.depends({
+  "react-responsive": "0.0.10",
+  "exposify": "0.5.0"
 });
